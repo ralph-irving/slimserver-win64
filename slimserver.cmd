f8209911083e8612c95719e824e3f53ca3d1bac5
Ralph Irving <ralph_irving@hotmail.com>
2022-03-06 12:05:32 -0500

    Rename run script.

--- slimserver.cmd
+++ slimserver.cmd
@@ -1,3 +1,3 @@
 @echo off
 set LOGDIR=C:\ProgramData\Squeezebox\Logs
-perl.exe "C:\Program Files\Squeezebox\slimserver\slimserver.pl" --prefsdir "C:\ProgramData\Squeezebox\prefs" --logdir "%LOGDIR%" --d_startup > "%LOGDIR%\outputlog.txt" 2> "%LOGDIR%\errorlog.txt"
+perl.exe "C:\Program Files\Squeezebox\server\slimserver.pl" --prefsdir "C:\ProgramData\Squeezebox\prefs" --logdir "%LOGDIR%" --d_startup > "%LOGDIR%\outputlog.txt" 2> "%LOGDIR%\errorlog.txt"
